9ff5ac4642a5f90563f16955c561173199d68e7d
Daniel Ogunseye <d.ogunseye7@gmail.com>
2022-04-22 13:06:30 +0100

    First commit

---
+++ portfolio/js/main.js
@@ -0,0 +1,11 @@
+//Animates Smooth Scroll
+$('#view-work').on('click', function () {
+  const images = $('images').position().top;
+
+  $('html, body').animate(
+    {
+      scrollTop: images,
+    },
+    900
+  );
+});
